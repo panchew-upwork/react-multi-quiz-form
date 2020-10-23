17c694064be409a07089455ac0bd71b9599671d9
panchew-upwork <panchew.upwork@gmail.com>
2020-10-23 03:01:36 -0500

    initial commit

---
+++ src/api/quizQuestions.js
@@ -0,0 +1,76 @@
+var quizQuestions = [
+  {
+      question: "What franchise would you rather play a game from?",
+      type: "input",
+      answers: [
+          {
+              type: "Microsoft",
+              content: "Halo"
+          },
+          {
+              type: "Nintendo",
+              content: "Pokemon"
+          },
+          {
+              type: "Sony",
+              content: "Uncharted"
+          }
+      ]
+  },
+  {
+      question: "Which console would you prefer to play with friends?",
+      type: "text",
+      answers: [
+          {
+              type: "Microsoft",
+              content: "X-Box"
+          },
+          {
+              type: "Nintendo",
+              content: "Nintendo 64"
+          },
+          {
+              type: "Sony",
+              content: "Playstation 1"
+          }
+      ]
+  },
+  {
+      question: "Which of these racing franchises would you prefer to play a game from?",
+      type: "single",
+      answers: [
+          {
+              type: "Microsoft",
+              content: "Forza"
+          },
+          {
+              type: "Nintendo",
+              content: "Mario Kart"
+          },
+          {
+              type: "Sony",
+              content: "Gran Turismo"
+          }
+      ]
+  },
+  {
+      question: "Which of these games do you think is best?",
+      type: "multi",
+      answers: [
+          {
+              type: "Microsoft",
+              content: "BioShock"
+          },
+          {
+              type: "Nintendo",
+              content: "The Legend of Zelda: Ocarina of Time"
+          },
+          {
+              type: "Sony",
+              content: "Final Fantasy VII"
+          }
+      ]
+  }
+];
+
+export default quizQuestions;
